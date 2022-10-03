e4a888ba4c8dd2de43e970150127070ac121038d
Paulo Ferr <pauloferrti@gmail.com>
2022-10-03 17:55:28 -0400

    c3 - create broadcast packet and combine with arp request

--- net-scan.py
+++ net-scan.py
@@ -2,6 +2,12 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+
+    # combine arp request and broadcast packet
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
 
 print(scan("10.0.2.1/24"))
