3a847d4753fe2e02975bfce008135921589a7ef6
Paulo Ferr <pauloferrti@gmail.com>
2022-10-07 16:36:15 -0400

    c4 - send broadcast requests and get answers

--- net-scan.py
+++ net-scan.py
@@ -2,12 +2,14 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
+    #arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
+    #broadcast.show()
 
     # combine arp request and broadcast packet
     arp_request_broadcast = broadcast/arp_request
-    arp_request_broadcast.show()
+    #arp_request_broadcast.show()
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 print(scan("10.0.2.1/24"))
